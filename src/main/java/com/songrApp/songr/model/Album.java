b53c9f17ca6c128bb88261af09dfc9b20496140b
malkhaleel88 <m.alkhaleel@yahoo.com>
2021-10-11 01:21:08 +0300

    update lab11

--- src/main/java/com/songrApp/songr/model/Album.java
+++ src/main/java/com/songrApp/songr/model/Album.java
@@ -8,11 +8,11 @@ public class Album {
     private String title;
     private String artist;
     private int songCount;
-    private int length;
+    private long length;
     private String imageUrl;
 
 
-    public Album(String title, String artist, int songCount, int length, String imageUrl) {
+    public Album(String title, String artist, int songCount, long length, String imageUrl) {
         this.title = title;
         this.artist = artist;
         this.songCount = songCount;
@@ -44,11 +44,11 @@ public class Album {
         this.songCount = songCount;
     }
 
-    public int getLength() {
+    public long getLength() {
         return length;
     }
 
-    public void setLength(int length) {
+    public void setLength(long length) {
         this.length = length;
     }
 
--- src/test/java/com/songrApp/songr/SongrApplicationTests.java
+++ src/test/java/com/songrApp/songr/SongrApplicationTests.java
@@ -1,13 +1,27 @@
 package com.songrApp.songr;
 
+import com.songrApp.songr.model.Album;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class SongrApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testConstructor() {
+		Album album1 = new Album("Tamally Maak", "Amr Diab", 10, 1500,"https://img.discogs.com/mrVhxSpktswpKo82TcPqv0vK80U=/fit-in/300x300/filters:strip_icc():format(webp):mode_rgb():quality(40)/discogs-images/R-6944106-1500743368-8340.jpeg.jpg");
+		album1.setTitle("Aktar Wahed");
+		assertEquals("Aktar Wahed",album1.getTitle());
+		album1.setArtist("Amr Diab");
+		assertEquals("Amr Diab",album1.getArtist());
+		album1.setSongCount(9);
+		assertEquals(9,album1.getSongCount());
+		album1.setLength(3000);
+		assertEquals(3000 , album1.getLength());
+		album1.setImageUrl("https://img.discogs.com/60vmnABbNLDfwLMosvcyjPsa-cE=/fit-in/600x605/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-10333312-1495486487-7127.jpeg.jpg");
+		assertEquals("https://img.discogs.com/60vmnABbNLDfwLMosvcyjPsa-cE=/fit-in/600x605/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-10333312-1495486487-7127.jpeg.jpg",album1.getImageUrl());
 	}
 
 }
